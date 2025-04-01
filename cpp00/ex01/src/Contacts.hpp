3ec324cdc75ae59e8a7de575dc478cdccb486673
Eduardo Castro <eduardocastrobritoo@gmail.com>
2025-04-01 19:50:45 -0300

    init contacts class

--- cpp00/ex01/src/Contacts.hpp
+++ cpp00/ex01/src/Contacts.hpp
@@ -1,30 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Contacts.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: educastro <educastro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/25 11:43:31 by educastro         #+#    #+#             */
-/*   Updated: 2025/03/25 11:43:32 by educastro        ###   ########.fr       */
+/*   Created: 2025/03/25 11:43:44 by educastro         #+#    #+#             */
+/*   Updated: 2025/03/25 17:20:51 by educastro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#ifndef CONTACTS_H
+# define CONTACTS_H
 
-int main(int ac, char **av)
-{
-    if (ac == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    else
-    {
-        for (int i = 1 ; av[i] ; i++)
-        {
-            for (int j = 0 ; av[i][j] ; j++)
-            {
-                std::cout << static_cast<char>(std::toupper(av[i][j]));
-            }
-        }
-        std::cout << std::endl;
-    }
-}
+#endif
--- cpp00/ex01/src/PhoneBook.hpp
+++ cpp00/ex01/src/PhoneBook.hpp
@@ -1,30 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: educastro <educastro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/25 11:43:31 by educastro         #+#    #+#             */
-/*   Updated: 2025/03/25 11:43:32 by educastro        ###   ########.fr       */
+/*   Created: 2025/03/25 11:43:48 by educastro         #+#    #+#             */
+/*   Updated: 2025/03/25 14:34:38 by educastro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
 
-int main(int ac, char **av)
-{
-    if (ac == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    else
-    {
-        for (int i = 1 ; av[i] ; i++)
-        {
-            for (int j = 0 ; av[i][j] ; j++)
-            {
-                std::cout << static_cast<char>(std::toupper(av[i][j]));
-            }
-        }
-        std::cout << std::endl;
-    }
-}
+# include <iostream>
+# include <string.h>
+
+#endif
